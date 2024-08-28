7b60af5516c9cd72fd3764e00223cf59906ae157
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-28 22:25:40 +0530

    added update user info  newPassword

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,57 +1,61 @@
-import { UserModel } from '../model/user.model.js'
-import bcrypt from 'bcryptjs'
+import { UserModel } from "../model/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const getUser = async (req, res) => {
-    try {
-        const userId = req.user.user
-        const user = await UserModel.findById(userId)
-        if (!user) return res.status(404).json({ message: "User not found" })
+  try {
+    const userId = req.user.user;
+    const user = await UserModel.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-        const userResponse = {
-            _id: user._id,
-            email: user.email,
-            userName: user.userName
-        }
+    const userResponse = {
+      _id: user._id,
+      email: user.email,
+      userName: user.userName,
+    };
 
-        return res.status(200).json({ message: "User response", user: userResponse })
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error during the user retrieval" })
-    }
-}
+    return res
+      .status(200)
+      .json({ message: "User response", user: userResponse });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error during the user retrieval" });
+  }
+};
 
 export const UpdateUserProfile = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const { userName, password } = req.body;
+  try {
+    const id = req.params.id;
+    const { password, newPassword } = req.body;
 
-        // Prepare the update object
-        let updateData = { userName };
+    const user = await UserModel.findById(id);
 
-        // Only hash the password if it is provided
-        if (password) {
-            const salt = await bcrypt.genSalt(12);
-            const hashPassword = await bcrypt.hash(password, salt);
-            updateData.password = hashPassword;
-        }
+    if (!user) return res.status(404).json({ message: "user not found" });
 
-        // Update the user
-        const user = await UserModel.findByIdAndUpdate(id, updateData, { new: true });
-
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const userResponse = {
-            _id: user._id,
-            email: user.email,
-            userName: user.userName
-        };
-
-        return res.status(200).json({ message: "User updated successfully", user: userResponse });
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error during updating the user" });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(403).json({ message: "Invalid password" });
     }
+    const salt = await bcrypt.genSalt(12);
+    const hashPassword = await bcrypt.hash(newPassword, salt);
+
+    user.password = hashPassword;
+    await user.save();
+
+    const userResponse = {
+      _id: user._id,
+      email: user.email,
+      userName: user.userName,
+    };
+
+    return res
+      .status(200)
+      .json({ message: "User updated successfully", user: userResponse });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error during updating the user" });
+  }
 };
--- routers/user.route.js
+++ routers/user.route.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import { VerifyToken } from '../utils/verifyToken.js'
-import { getUser, UpdateUserProfile } from '../controllers/user.controller.js'
+import express from "express";
+import { VerifyToken } from "../utils/verifyToken.js";
+import { getUser, UpdateUserProfile } from "../controllers/user.controller.js";
 
-export const UserRouter = express.Router()
+export const UserRouter = express.Router();
 
-UserRouter.get('/', VerifyToken, getUser)
-UserRouter.patch('/:id', UpdateUserProfile)
+UserRouter.get("/", VerifyToken, getUser);
+UserRouter.patch("/:id", VerifyToken, UpdateUserProfile);
