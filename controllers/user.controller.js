f1af4ffb8ef91c957bf2dfb93e1220a954eb9c9b
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-28 18:13:50 +0530

    creaeted update user route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { UserModel } from '../model/user.model.js'
+import bcrypt from 'bcryptjs'
 
 export const getUser = async (req, res) => {
     try {
@@ -18,3 +19,39 @@ export const getUser = async (req, res) => {
         return res.status(500).json({ message: "Internal server error during the user retrieval" })
     }
 }
+
+export const UpdateUserProfile = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { userName, password } = req.body;
+
+        // Prepare the update object
+        let updateData = { userName };
+
+        // Only hash the password if it is provided
+        if (password) {
+            const salt = await bcrypt.genSalt(12);
+            const hashPassword = await bcrypt.hash(password, salt);
+            updateData.password = hashPassword;
+        }
+
+        // Update the user
+        const user = await UserModel.findByIdAndUpdate(id, updateData, { new: true });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const userResponse = {
+            _id: user._id,
+            email: user.email,
+            userName: user.userName
+        };
+
+        return res.status(200).json({ message: "User updated successfully", user: userResponse });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error during updating the user" });
+    }
+};
