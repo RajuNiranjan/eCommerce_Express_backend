5887cc2b703742f48e40b971af194973a4799ea4
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-06 14:24:33 +0530

    updated create product route

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -16,17 +16,19 @@ export const CreateProduct = async (req, res) => {
         storeId
     } = req.body;
 
+    const parsedPrice = parseFloat(price);
+    const parsedOfferPrice = parseFloat(offerPrice);
+    const parsedStockLevel = parseInt(stockLevel);
+
 
     if (
         !productName ||
         !description ||
         !categories ||
-        !Array.isArray(categories) ||
-        !size ||
         !Array.isArray(size) ||
-        typeof price !== 'number' ||
-        typeof offerPrice !== 'number' ||
-        typeof stockLevel !== 'number' ||
+        isNaN(parsedPrice) ||
+        isNaN(parsedOfferPrice) ||
+        isNaN(parsedStockLevel) ||
         !images ||
         !Array.isArray(images) ||
         !colors ||
@@ -38,31 +40,50 @@ export const CreateProduct = async (req, res) => {
     }
 
     try {
-
         const newProduct = new ProductModel({
             productName,
             description,
             categories,
             size,
-            price,
-            offerPrice,
-            stockLevel,
+            price: parsedPrice,
+            offerPrice: parsedOfferPrice,
+            stockLevel: parsedStockLevel,
             images,
             colors,
             userId,
             storeId
         });
 
-        const store = await BecomeSellerModel.findOne({ _id: storeId })
+        const store = await BecomeSellerModel.findOne({ _id: storeId });
 
-        if (!store) return res.status(404).json({ message: "Store not found" })
+        if (!store) {
+            return res.status(404).json({ message: "Store not found" });
+        }
 
         const savedProduct = await newProduct.save();
-
 
         return res.status(201).json({ message: "Product created successfully", product: savedProduct });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Internal server error during creating product" });
     }
+};
+
+
+export const getProductsByStoreId = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const products = await ProductModel.find({ storeId: id });
+        if (products.length === 0) {
+            return res.status(404).json({ message: "No products found for the given storeId." });
+        }
+        return res.status(200).json({
+            message: "Products data fetched successfully",
+            products: products
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error while fetching products." });
+    }
 }
+
--- routers/product.route.js
+++ routers/product.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { CreateProduct } from '../controllers/product.controller.js'
+import { CreateProduct, getProductsByStoreId } from '../controllers/product.controller.js'
 import { VerifyToken } from '../utils/verifyToken.js'
 
 
 export const ProductRouter = express.Router()
 
 ProductRouter.post('/', VerifyToken, CreateProduct)
+ProductRouter.get('/:id', VerifyToken, getProductsByStoreId)
