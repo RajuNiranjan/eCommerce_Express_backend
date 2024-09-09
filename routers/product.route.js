8076644793eb7b381f77fca53f4120426cf145f8
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-10 00:08:01 +0530

    added edit prouduct route

--- routers/product.route.js
+++ routers/product.route.js
@@ -1,12 +1,19 @@
-import express from 'express'
-import { CreateProduct, getProductsByStoreId, DeleteProduct, GetAllProducts, GetProductById } from '../controllers/product.controller.js'
-import { VerifyToken } from '../utils/verifyToken.js'
+import express from "express";
+import {
+  CreateProduct,
+  getProductsByStoreId,
+  DeleteProduct,
+  GetAllProducts,
+  GetProductById,
+  EditProduct,
+} from "../controllers/product.controller.js";
+import { VerifyToken } from "../utils/verifyToken.js";
 
+export const ProductRouter = express.Router();
 
-export const ProductRouter = express.Router()
-
-ProductRouter.post('/', VerifyToken, CreateProduct)
-ProductRouter.get('/:id', VerifyToken, getProductsByStoreId)
-ProductRouter.delete("/:id", VerifyToken, DeleteProduct)
-ProductRouter.get('/', GetAllProducts)
-ProductRouter.get('/singel_product/:id', GetProductById)
+ProductRouter.post("/", VerifyToken, CreateProduct);
+ProductRouter.get("/:id", VerifyToken, getProductsByStoreId);
+ProductRouter.delete("/:id", VerifyToken, DeleteProduct);
+ProductRouter.get("/", GetAllProducts);
+ProductRouter.get("/singel_product/:id", GetProductById);
+ProductRouter.patch("/:id", VerifyToken, EditProduct);
