e1339c0a1e1b9000398bf0af32d88fe38c41cf80
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-12 21:55:01 +0530

    updated get cart items route

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,61 +1,88 @@
-import { CartModel } from '../model/cart.model.js';
-import { ProductModel } from '../model/product.model.js';
+import { CartModel } from "../model/cart.model.js";
+import { ProductModel } from "../model/product.model.js";
 
 export const AddToCart = async (req, res) => {
-    const id = req.user.user
-    const { userId, productId, quantity, color, size } = req.body
+  const id = req.user.user;
+  const { userId, productId, quantity, color, size } = req.body;
 
-    try {
-        if (id !== userId) {
-            return res.status(403).json({ message: "Invalid user" })
-        }
-
-        const existingProduct = await CartModel.findOne({ productId })
-
-        if (!existingProduct) {
-            const product = await ProductModel.findById(productId)
-            if (!product) return res.status(404).json({ message: "Product not found" })
-
-            const cartItem = new CartModel({
-                productId,
-                userId,
-                quantity,
-                size,
-                color
-            })
-            await cartItem.save()
-            return res.status(200).json({ message: "Product Added To cart Successfully" })
-        } else {
-            return res.status(409).json({ message: "Product already existed in cart" })
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error" })
+  try {
+    if (id !== userId) {
+      return res.status(403).json({ message: "Invalid user" });
     }
-}
+
+    const existingProduct = await CartModel.findOne({ productId });
+
+    if (!existingProduct) {
+      const product = await ProductModel.findById(productId);
+      if (!product)
+        return res.status(404).json({ message: "Product not found" });
+
+      const cartItem = new CartModel({
+        productId,
+        userId,
+        quantity,
+        size,
+        color,
+      });
+      await cartItem.save();
+      return res
+        .status(200)
+        .json({ message: "Product Added To cart Successfully" });
+    } else {
+      return res
+        .status(409)
+        .json({ message: "Product already existed in cart" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const GetProducts = async (req, res) => {
-    const id = req.user.user
-    const { userId } = req.params
-    try {
-        if (id !== userId) return res.status(404).json({ message: "Invalid user" })
-        const cartItems = await CartModel.find({ userId })
-        return res.status(200).json({ message: "Cart Items Fetched Successfully", cartItems: cartItems })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error" })
-    }
-}
+  const id = req.user.user;
+  const { userId } = req.params;
+  try {
+    if (id !== userId) return res.status(404).json({ message: "Invalid user" });
+    const cartItems = await CartModel.find({ userId });
+
+    const productIds = cartItems.map((item) => item.productId);
+    const products = await ProductModel.find({ _id: { $in: productIds } });
+
+    const cartDetails = cartItems.map((cartItem) => {
+      const product = products.find((prod) =>
+        prod._id.equals(cartItem.productId)
+      );
+      return {
+        product,
+        size: cartItem.size,
+        color: cartItem.color,
+        quantity: cartItem.quantity,
+      };
+    });
+
+    return res.status(200).json({
+      message: "Cart Items Fetched Successfully",
+      cartItems: cartDetails,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const RemoveFromCart = async (req, res) => {
-    const { userId, productId } = req.params
+  const { userId, productId } = req.params;
 
-    try {
-        const removeFromCart = await CartModel.deleteOne({ userId, productId })
-        if (removeFromCart.deletedCount === 0) return res.status(404).json({ message: "Cart item not found" })
-        return res.status(200).json({ message: "Item removed from cart successfully" })
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error" })
-    }
-}
+  try {
+    const removeFromCart = await CartModel.deleteOne({ userId, productId });
+    if (removeFromCart.deletedCount === 0)
+      return res.status(404).json({ message: "Cart item not found" });
+    return res
+      .status(200)
+      .json({ message: "Item removed from cart successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- controllers/wishList.controller.js
+++ controllers/wishList.controller.js
@@ -1,70 +1,86 @@
-import { WishListModel } from '../model/wishList.model.js'
-import { ProductModel } from '../model/product.model.js'
-
+import { WishListModel } from "../model/wishList.model.js";
+import { ProductModel } from "../model/product.model.js";
 
 export const AddToWishList = async (req, res) => {
-    const id = req.user.user
-    const { userId, productId } = req.body
-    try {
+  const id = req.user.user;
+  const { userId, productId } = req.body;
+  try {
+    if (id === userId) {
+      const existedWishListProduct = await WishListModel.findOne({
+        userId,
+        productId,
+      });
 
-        if (id === userId) {
-
-            const existedWishListProduct = await WishListModel.findOne({ userId, productId })
-
-            if (!existedWishListProduct) {
-                const product = await ProductModel.findById(productId)
-                if (!product) return res.status(404).json({ message: "Product not found" })
-                const wishListItem = new WishListModel({ userId, productId })
-                await wishListItem.save()
-                return res.status(200).json({ message: "Product added to wish list successfully" })
-            } else {
-                return res.status(403).json({ message: "Product already existed in wish list" })
-            }
-        } else {
-            return res.status(401).json({ message: "Unauthorized access" })
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error" })
+      if (!existedWishListProduct) {
+        const product = await ProductModel.findById(productId);
+        if (!product)
+          return res.status(404).json({ message: "Product not found" });
+        const wishListItem = new WishListModel({ userId, productId });
+        await wishListItem.save();
+        return res
+          .status(200)
+          .json({ message: "Product added to wish list successfully" });
+      } else {
+        return res
+          .status(403)
+          .json({ message: "Product already existed in wish list" });
+      }
+    } else {
+      return res.status(401).json({ message: "Unauthorized access" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const GetWishListItems = async (req, res) => {
-    const id = req.user.user
-    const { userId } = req.params
+  const id = req.user.user;
+  const { userId } = req.params;
 
-    try {
-        if (id === userId) {
-            const wishListItems = await WishListModel.find({ userId })
-            if (!wishListItems) return res.status(404).json({ message: "No wishList items found" })
+  try {
+    if (id === userId) {
+      const wishListItems = await WishListModel.find({ userId });
+      if (!wishListItems)
+        return res.status(404).json({ message: "No wishList items found" });
 
-            const productIds = wishListItems.map(item => item.productId)
-            const products = await ProductModel.find({ _id: { $in: productIds } })
+      const productIds = wishListItems.map((item) => item.productId);
+      const products = await ProductModel.find({ _id: { $in: productIds } });
 
-            return res.status(200).json({ message: "Wishlist fetched successfully", wishList: products })
-        } else {
-            return res.status(401).json({ message: "Unauthorized access" });
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error during fetching the wish list" })
+      return res
+        .status(200)
+        .json({ message: "Wishlist fetched successfully", wishList: products });
+    } else {
+      return res.status(401).json({ message: "Unauthorized access" });
     }
-}
-
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error during fetching the wish list" });
+  }
+};
 
 export const removeFromWishList = async (req, res) => {
-    const user = req.user.user
-    const { userId, productId } = req.params
-    try {
-        if (user === userId) {
-            const result = await WishListModel.deleteOne({ userId, productId })
-            if (result.deletedCount === 0) return res.status(404).json({ message: "WishList item not found" })
-            return res.status(200).json({ message: "WishList item removed successfully" })
-        } else {
-            return res.status(401).json({ message: "Unauthorized access" })
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal server error during removing from the wish list" })
+  const user = req.user.user;
+  const { userId, productId } = req.params;
+  try {
+    if (user === userId) {
+      const result = await WishListModel.deleteOne({ userId, productId });
+      if (result.deletedCount === 0)
+        return res.status(404).json({ message: "WishList item not found" });
+      return res
+        .status(200)
+        .json({ message: "WishList item removed successfully" });
+    } else {
+      return res.status(401).json({ message: "Unauthorized access" });
     }
-}
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({
+        message: "Internal server error during removing from the wish list",
+      });
+  }
+};
