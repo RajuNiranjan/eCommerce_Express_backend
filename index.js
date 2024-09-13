8b461b8b07884ed0ba8778ed4a70886b04f2d2b8
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-13 16:38:46 +0530

    disabled the cors

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const corsOptions = {
 }
 
 app.use(express.json())
-app.use(cors(corsOptions))
-// app.use(cors())
+// app.use(cors(corsOptions))
+app.use(cors())
 
 
 app.get('/', (req, res) => {
